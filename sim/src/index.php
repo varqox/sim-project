ba3a497dfb800b93eee8d88be171f7e4a9d48260
Krzysztof Małysa <krzyk240@gmail.com>
2013-12-26 15:22:15 +0100

    Add logo

--- sim/src/index.php
+++ sim/src/index.php
@@ -7,6 +7,7 @@ require_once $_SERVER['DOCUMENT_ROOT']."/kit/main.php";
 template_begin('Wysyłanie rozwiązań',$user);
 
 echo '<div style="text-align: center">
+<img src="/kit/img/SIM-logo.png" width="20%" height=20%"/>
 <p style="font-size: 30px">W trakcie tworzenia...</p>
 <hr/>
 <p>Witam na stronie głównej serwisu SIM używanego do przeprowadzania konkursów w programowaniu</p>
