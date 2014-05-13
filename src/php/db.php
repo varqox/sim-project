9f5b59b7f425ace007ccfc1cb7559fe8d3b838d0
Krzysztof Małysa <krzyk240@gmail.com>
2014-05-13 14:26:47 +0200

    Secure bug fixed

--- src/php/db.php
+++ src/php/db.php
@@ -11,7 +11,15 @@ class DB
 
 	private function __construct()
 	{
-		$this->pdo = new PDO('mysql:host='.mysql_host.';dbname='.database, mysql_username, mysql_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		try
+		{
+			$this->pdo = new PDO('mysql:host='.mysql_host.';dbname='.database, mysql_username, mysql_password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		}
+		catch(PDOException $e)
+		{
+			echo "DB connection error!";
+			exit;
+		}
 	}
 
 	static public function pdo()
