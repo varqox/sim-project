518b729b83902c0bd8d0b24025d802fde4d71029
Krzysztof Małysa <krzyk240@gmail.com>
2015-06-24 00:16:58 +0200

    Add round editing, isDatetimeOrBlank(), improve and correct CSS styles, improve forms

--- simlib/include/time.h
+++ simlib/include/time.h
@@ -8,4 +8,8 @@ long long microtime();
 std::string date(const std::string& str, time_t cur_time = -1);
 
 // Checks if format is "%Y-%m-%d %H:%M:%S"
-bool is_datetime(const std::string& str);
+bool isDatetime(const std::string& str);
+
+inline bool isDatetimeOrBlank(const std::string& str) {
+	return str.empty() || isDatetime(str);
+}
