b87cfd28672d7eeac5994d674aa75cd255346762
Krzysztof Małysa <krzyk240@gmail.com>
2015-12-28 20:14:24 +0100

    A few improvements site clock, update src/simlib

--- sim/src/public/kit/scripts.js
+++ sim/src/public/kit/scripts.js
@@ -1,16 +1,7 @@
-window.onload = function updateClock()
-{
-	if (typeof updateClock.time_difference === 'undefined') {
+window.onload = function updateClock() {
+	if (typeof updateClock.time_difference === 'undefined')
 		updateClock.time_difference = window.performance.timing.responseStart - start_time;
-		// console.log('diff:   ' + (updateClock.time_difference));
-		// console.log('date:   ' + ((new Date()).getTime()));
-		// console.log('server: ' + ((new Date()).getTime() - updateClock.time_difference));
-		// console.log('start:  ' + (start_time));
-		// console.log('load:   ' + ((new Date()).getTime() - window.performance.timing.responseStart));
-		// console.log('end:    ' + (window.performance.timing.domContentLoadedEventEnd));
-		// console.log('begin:  ' + (window.performance.timing.responseStart));
-		// console.log(window.performance);
-	}
+
 	var time = new Date();
 	time.setTime(time.getTime() - updateClock.time_difference);
 	var hours = time.getUTCHours();
@@ -21,20 +12,19 @@ window.onload = function updateClock()
 	seconds = (seconds < 10 ? '0' : '') + seconds;
 	// Update the displayed time
 	document.getElementById('clock').innerHTML = String().concat(hours, ':', minutes, ':', seconds, ' UTC');
-	setTimeout(function(){ updateClock() }, 1000 - time.getMilliseconds());
+	setTimeout(updateClock, 1000 - time.getMilliseconds());
 }
 $(document).ready(function(){
-	$('.dropdown > .dropdown-toggle').click(function(event){
+	$('.dropdown > .dropdown-toggle').click(function(event) {
 		event.preventDefault();
 		if($(this).parent().is('.open'))
 			$(this).parent().removeClass('open');
-		else
-		{
+		else {
 			$('.dropdown.open').removeClass('open');
 			$(this).parent().addClass('open');
 		}
 	});
-	$(document).click(function(event){
+	$(document).click(function(event) {
 		if(!$(event.target).is('.dropdown-toggle, .dropdown-toggle *'))
 			$('.dropdown.open').removeClass('open');
 	});
