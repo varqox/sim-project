34643a04a404a4cf4d6f0769f1dc882c2eb3404c
Krzysztof Małysa <krzyk240@gmail.com>
2016-03-19 22:40:57 +0100

    Add useful exception macros

--- simlib/include/debug.h
+++ simlib/include/debug.h
@@ -5,13 +5,19 @@
 #define eprintf(...) fprintf(stderr, __VA_ARGS__)
 
 #ifdef DEBUG
-
 # define D(...) __VA_ARGS__
-# define E(...) eprintf(__VA_ARGS__)
-
 #else
-
 # define D(...)
-# define E(...)
-
 #endif
+
+#define E(...) eprintf(__VA_ARGS__)
+
+// Very useful - includes exception origin
+#define THROW(...) throw std::runtime_error(concat(__VA_ARGS__, " (thrown at " \
+	__FILE__ ":", toString(__LINE__), ')'))
+
+#define ERRLOG_CATCH() errlog(__FILE__ ":", toString(__LINE__), \
+	": Caught exception")
+
+#define ERRLOG_CAUGHT(e) errlog(__FILE__ ":", toString(__LINE__), \
+	": Caught exception -> ", e.what())
