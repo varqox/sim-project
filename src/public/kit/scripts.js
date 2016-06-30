d5f0724d6decfdb6bf8776f921d894d70c9e76a6
Krzysztof Małysa <krzyk240@gmail.com>
2016-06-30 23:54:29 +0200

    Improved navigation and added button to select the full code on 'Submission source' page

--- src/public/kit/scripts.js
+++ src/public/kit/scripts.js
@@ -50,3 +50,18 @@ $(function() {
 			getCookie('csrf_token') + '">');
 	});
 });
+
+function SelectText(element) {
+    var doc = document, range, selection;
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(element);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(element);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
