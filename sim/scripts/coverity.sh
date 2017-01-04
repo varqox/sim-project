b40c731fdf8a419b15adda9be3e9d5d03bfd00aa
Krzysztof Małysa <krzyk240@gmail.com>
2017-01-04 22:38:32 +0100

    Coverity 8.7.0 works well again with clang (at least the version 3.8)

--- sim/scripts/coverity.sh
+++ sim/scripts/coverity.sh
@@ -4,8 +4,4 @@
 rm -rf cov-int/
 rm -f sim-cov-build.tar.gz
 make clean
-cp src/include/simlib/meta.h meta.h~
-echo '#define static_assert(...)' >> src/include/simlib/meta.h
-cov-build --dir cov-int make -j $(grep -c ^processor /proc/cpuinfo) DEBUG=5 CC=gcc CXX=g++ test && tar caf sim-cov-build.xz cov-int
-rm src/include/simlib/meta.h
-mv meta.h~ src/include/simlib/meta.h
+cov-build --dir cov-int make -j $(grep -c ^processor /proc/cpuinfo) DEBUG=5 CC=clang CXX=clang++ test && tar caf sim-cov-build.xz cov-int
