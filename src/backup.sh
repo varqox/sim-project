df143d2d1f9c4b1bceb1156b6eef846ccbe01dba
Krzysztof Małysa <krzyk240@gmail.com>
2017-02-26 15:26:03 +0100

    Added src/backup.sh

---
+++ src/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "[client]" > .mysql.cnf
+chmod 0600 .mysql.cnf
+echo "user=$(head -n 1 .db.config)" >> .mysql.cnf
+echo "password=$(head -n 2 .db.config | tail -n 1)" >> .mysql.cnf
+db=$(head -n 3 .db.config | tail -n 1)
+mysqldump --defaults-file=.mysql.cnf --result-file=dump.sql --extended-insert=FALSE $db
+
+git init
+git config --local user.name "Sim backuper"
+git add solutions dump.sql
+git commit -m "Backup $(date '+%Y-%m-%d %H:%M:%S')"
+
+rm dump.sql
+rm .mysql.cnf
