53e3a927aced5b509638775b2180f4df35af597d
Krzysztof Małysa <krzyk240@gmail.com>
2017-04-26 19:11:22 +0200

    Fixed linking errors

--- simlib/src/debug.cc
+++ simlib/src/debug.cc
@@ -1,12 +1,5 @@
 #include "../include/debug.h"
 
-extern "C" int __cxa_thread_atexit(void (*func)(void*), void *obj,
-	void *dso_symbol)
-{
-	int __cxa_thread_atexit_impl(void (*)(void*), void *, void *);
-	return __cxa_thread_atexit_impl(func, obj, dso_symbol);
-}
-
 namespace stack_unwinding {
 thread_local InplaceArray<InplaceBuff<256>, 32> marks_collected;
 } // stack_unwinding
