050519046064cd12af268cb3044a170b76de5231
Krzysztof Małysa <krzyk240@gmail.com>
2017-09-09 18:33:52 +0200

    meta.h: Added macro DECLARE_ENUM_ASSIGN_OPERATOR()

--- simlib/include/meta.h
+++ simlib/include/meta.h
@@ -178,6 +178,13 @@ inline size_t constexpr offset_of(T1 T2::*member) {
 		return static_cast<enu>(static_cast<UT>(a) oper static_cast<UT>(b)); \
 	}
 
+#define DECLARE_ENUM_ASSIGN_OPERATOR(enu, oper) \
+	constexpr inline enu& operator oper(enu& a, enu b) { \
+		using UT = std::underlying_type<enu>::type; \
+		UT x = static_cast<UT>(a); \
+		return (a = static_cast<enu>(x oper static_cast<UT>(b))); \
+	}
+
 #define DECLARE_ENUM_UNARY_OPERATOR(enu, oper) \
 	constexpr inline enu operator oper(enu a) { \
 		using UT = std::underlying_type<enu>::type; \
