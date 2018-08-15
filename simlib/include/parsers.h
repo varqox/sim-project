0bdd0a304361773dd2068bdaa4cb75a6aa1c1c20
Krzysztof Małysa <krzyk240@gmail.com>
2018-08-15 12:36:20 +0200

    Added ArgvParser;

--- simlib/include/parsers.h
+++ simlib/include/parsers.h
@@ -126,3 +126,34 @@ public:
 
 	constexpr StringView remnant() const noexcept { return buff; }
 };
+
+class ArgvParser {
+	uint argc_;
+	char** argv_;
+
+public:
+	ArgvParser(int argc, char** argv) : argc_(std::max(argc - 1, 0)), argv_(argv + 1) {}
+
+	ArgvParser(const ArgvParser&) = default;
+	ArgvParser(ArgvParser&&) noexcept = default;
+	ArgvParser& operator=(const ArgvParser&) = default;
+	ArgvParser& operator=(ArgvParser&&) noexcept = default;
+
+	~ArgvParser() = default;
+
+	CStringView next() const noexcept { return operator[](0); }
+
+	CStringView operator[](uint n) const noexcept {
+		return (n < argc_ ? CStringView(argv_[n]) : CStringView());
+	}
+
+	uint size() const noexcept { return argc_; }
+
+	CStringView extract_next() noexcept {
+		if (argc_ > 0) {
+			--argc_;
+			return CStringView(argv_++[0]);
+		} else
+			return {};
+	}
+};
