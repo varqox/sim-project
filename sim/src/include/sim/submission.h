23bb3ec798293d7ba9a4b3b1a3ffbf0a9dfdf116
Krzysztof Małysa <krzyk240@gmail.com>
2018-10-28 15:36:31 +0100

    Updated simlib

--- sim/src/include/sim/submission.h
+++ sim/src/include/sim/submission.h
@@ -4,6 +4,7 @@
 
 namespace submission {
 
+// TODO: owner and contest_problem_id should be an Optional
 void update_final(MySQL::Connection& mysql, StringView submission_owner,
 	StringView problem_id, StringView contest_problem_id,
 	bool lock_tables = true);
