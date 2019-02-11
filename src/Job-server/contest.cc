c9446e6faea868d94bd38ec21e1bb0048505ab56
Krzysztof Małysa <krzyk240@gmail.com>
2019-02-11 14:27:21 +0100

    Added some info about deleted object to the job log

--- src/Job-server/contest.cc
+++ src/Job-server/contest.cc
@@ -32,6 +32,32 @@ void contest_problem_reselect_final_submissions(uint64_t job_id,
 }
 
 void delete_contest(uint64_t job_id, StringView contest_id) {
+	InplaceBuff<256> job_log;
+
+	auto set_failure = [&](auto&&... args) {
+		job_log.append(std::forward<decltype(args)>(args)...);
+		auto stmt = mysql.prepare("UPDATE jobs"
+			" SET status=" JSTATUS_FAILED_STR ", data=?"
+			" WHERE id=? AND status!=" JSTATUS_CANCELED_STR);
+		stmt.bindAndExecute(job_log, job_id);
+
+		stdlog("Job ", job_id, ":\n", job_log);
+	};
+
+	// Log some info about the deleted contest
+	{
+		auto stmt = mysql.prepare("SELECT name FROM contests WHERE id=?");
+		stmt.bindAndExecute(contest_id);
+		InplaceBuff<32> cname;
+		stmt.res_bind_all(cname);
+		if (not stmt.next()) {
+			return set_failure("Contest with id: ", contest_id,
+			" does not exist");
+		}
+
+		job_log.append("Contest: ", cname, " (", contest_id, ")\n");
+	}
+
 	// Run deletion twice - because race condition may occur and e.g. user may
 	// add submission after the submissions were deleted (thus it would not be
 	// deleted). Other order e.g. removing contest first would expose corrupted
@@ -82,11 +108,42 @@ void delete_contest(uint64_t job_id, StringView contest_id) {
 	}
 
 	auto stmt = mysql.prepare("UPDATE jobs"
-		" SET status=" JSTATUS_DONE_STR ", data='' WHERE id=?");
-	stmt.bindAndExecute(job_id);
+		" SET status=" JSTATUS_DONE_STR ", data=? WHERE id=?");
+	stmt.bindAndExecute(job_log, job_id);
 }
 
 void delete_contest_round(uint64_t job_id, StringView contest_round_id) {
+	InplaceBuff<256> job_log;
+
+	auto set_failure = [&](auto&&... args) {
+		job_log.append(std::forward<decltype(args)>(args)...);
+		auto stmt = mysql.prepare("UPDATE jobs"
+			" SET status=" JSTATUS_FAILED_STR ", data=?"
+			" WHERE id=? AND status!=" JSTATUS_CANCELED_STR);
+		stmt.bindAndExecute(job_log, job_id);
+
+		stdlog("Job ", job_id, ":\n", job_log);
+	};
+
+	// Log some info about the deleted contest round
+	{
+		auto stmt = mysql.prepare("SELECT c.name, c.id, r.name"
+			" FROM contest_rounds r"
+			" JOIN contests c ON c.id=r.contest_id"
+			" WHERE r.id=?");
+		stmt.bindAndExecute(contest_round_id);
+		InplaceBuff<32> cname, cid, rname;
+		stmt.res_bind_all(cname, cid, rname);
+		if (not stmt.next()) {
+			return set_failure("Contest round with id: ", contest_round_id,
+				" does not exist or the contest hierarchy is broken (likely the"
+				" former).");
+		}
+
+		job_log.append("Contest: ", cname, " (", cid, ")\n"
+			"Contest round: ", rname, " (", contest_round_id, ")\n");
+	}
+
 	// Run deletion twice - because race condition may occur and e.g. user may
 	// add submission after the submissions were deleted (thus it would not be
 	// deleted). Other order e.g. removing contest round first would expose
@@ -113,11 +170,47 @@ void delete_contest_round(uint64_t job_id, StringView contest_round_id) {
 	}
 
 	auto stmt = mysql.prepare("UPDATE jobs"
-		" SET status=" JSTATUS_DONE_STR ", data='' WHERE id=?");
-	stmt.bindAndExecute(job_id);
+		" SET status=" JSTATUS_DONE_STR ", data=? WHERE id=?");
+	stmt.bindAndExecute(job_log, job_id);
 }
 
 void delete_contest_problem(uint64_t job_id, StringView contest_problem_id) {
+	InplaceBuff<256> job_log;
+
+	auto set_failure = [&](auto&&... args) {
+		job_log.append(std::forward<decltype(args)>(args)...);
+		auto stmt = mysql.prepare("UPDATE jobs"
+			" SET status=" JSTATUS_FAILED_STR ", data=?"
+			" WHERE id=? AND status!=" JSTATUS_CANCELED_STR);
+		stmt.bindAndExecute(job_log, job_id);
+
+		stdlog("Job ", job_id, ":\n", job_log);
+	};
+
+	// Log some info about the deleted contest problem
+	{
+		auto stmt = mysql.prepare("SELECT c.name, c.id, r.name, r.id, cp.name,"
+				" p.name, p.id"
+			" FROM contest_problems cp"
+			" JOIN contest_rounds r ON r.id=cp.contest_round_id"
+			" JOIN contests c ON c.id=cp.contest_id"
+			" JOIN problems p ON p.id=cp.problem_id"
+			" WHERE cp.id=?");
+		stmt.bindAndExecute(contest_problem_id);
+		InplaceBuff<32> cname, cid, rname, rid, cpname, pname, pid;
+		stmt.res_bind_all(cname, cid, rname, rid, cpname, pname, pid);
+		if (not stmt.next()) {
+			return set_failure("Contest problem with id: ", contest_problem_id,
+				" does not exist or the contest hierarchy is broken (likely the"
+				" former).");
+		}
+
+		job_log.append("Contest: ", cname, " (", cid, ")\n"
+			"Contest round: ", rname, " (", rid, ")\n"
+			"Contest problem: ", cpname, " (", contest_problem_id, ")\n"
+			"Attached problem: ", pname, " (", pid, ")\n");
+	}
+
 	// Run deletion twice - because race condition may occur and e.g. user may
 	// add submission after the submissions were deleted (thus it would not be
 	// deleted). Other order e.g. removing contest problem first would expose
@@ -140,6 +233,6 @@ void delete_contest_problem(uint64_t job_id, StringView contest_problem_id) {
 	}
 
 	auto stmt = mysql.prepare("UPDATE jobs"
-		" SET status=" JSTATUS_DONE_STR ", data='' WHERE id=?");
-	stmt.bindAndExecute(job_id);
+		" SET status=" JSTATUS_DONE_STR ", data=? WHERE id=?");
+	stmt.bindAndExecute(job_log, job_id);
 }
