e441d4fc42dbea7c3fd1c6596bcb8ccbb0039fe9
Krzysztof Małysa <krzyk240@gmail.com>
2019-02-12 14:16:30 +0100

    Removed usage of O_LARGEFILE flag

--- src/web-interface/api.cc
+++ src/web-interface/api.cc
@@ -111,7 +111,7 @@ void Sim::api_logs() {
 			end_offset = 0;
 	}
 
-	FileDescriptor fd(filename, O_RDONLY | O_LARGEFILE);
+	FileDescriptor fd(filename, O_RDONLY);
 	off64_t fsize = lseek64(fd, 0, SEEK_END);
 	throw_assert(fsize >= 0);
 	if (que.empty())
