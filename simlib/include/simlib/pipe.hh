68ac3ef416c9fafc7920d6447d8065228544df62
Krzysztof Małysa <varqox@gmail.com>
2020-11-09 23:43:55 +0100

    Added pipe.hh with pipe2() wrapper

---
+++ simlib/include/simlib/pipe.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "file_descriptor.hh"
+
+#include <optional>
+#include <unistd.h>
+
+struct Pipe {
+	FileDescriptor readable;
+	FileDescriptor writable;
+};
+
+// Returns std::nullopt on error with errno set appropriately
+inline std::optional<Pipe> pipe2(int flags) noexcept {
+	int pfd[2];
+	int rc = pipe2(pfd, flags);
+	if (rc) {
+		return std::nullopt;
+	}
+	return Pipe{
+	   .readable = FileDescriptor{pfd[0]},
+	   .writable = FileDescriptor{pfd[1]},
+	};
+}
