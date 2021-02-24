d0b6e8a91393380cbcc8817ef890dcf8e92e0185
Krzysztof Małysa <varqox@gmail.com>
2021-02-25 00:47:12 +0100

    test: url_dispatcher.cc removed superfluous const

--- simlib/test/http/url_dispatcher.cc
+++ simlib/test/http/url_dispatcher.cc
@@ -22,7 +22,7 @@ static constexpr std::optional<std::tuple<>> is_xyz(StringView str) {
 // NOLINTNEXTLINE
 TEST(http, UrlParser) {
     using http::UrlParser;
-    static constexpr const char url[] = "/{i64}/abcxyz/{i64}/{string}/{u64}/{custom}";
+    static constexpr char url[] = "/{i64}/abcxyz/{i64}/{string}/{u64}/{custom}";
     static_assert(UrlParser<url, is_xyz>::literal_prefix == "/");
     static_assert(UrlParser<url, is_xyz>::literal_suffix == "");
     // Parsing
@@ -35,34 +35,33 @@ TEST(http, UrlParser) {
                   typename UrlParser<url, is_xyz>::HandlerArgsTuple,
                   std::tuple<int64_t, int64_t, StringView, uint64_t, std::tuple<>>>);
     // Prefixes and suffixes
-    static constexpr const char url1[] =
-        "/a/bc/def/hijk/{i64}/abcxyz/{i64}/{string}/{u64}/{custom}";
+    static constexpr char url1[] = "/a/bc/def/hijk/{i64}/abcxyz/{i64}/{string}/{u64}/{custom}";
     static_assert(UrlParser<url1, is_xyz>::literal_prefix == "/a/bc/def/hijk/");
     static_assert(UrlParser<url1, is_xyz>::literal_suffix == "");
-    static constexpr const char url2[] =
+    static constexpr char url2[] =
         "/a/bc/def/hijk/{i64}/abcxyz/{i64}/{string}/{u64}/{custom}/";
     static_assert(UrlParser<url2, is_xyz>::literal_prefix == "/a/bc/def/hijk/");
     static_assert(UrlParser<url2, is_xyz>::literal_suffix == "/");
-    static constexpr const char url3[] =
+    static constexpr char url3[] =
         "/a/bc/def/hijk/{i64}/abcxyz/{i64}/{string}/{u64}/{custom}/x/yz/okl/";
     static_assert(UrlParser<url3, is_xyz>::literal_prefix == "/a/bc/def/hijk/");
     static_assert(UrlParser<url3, is_xyz>::literal_suffix == "/x/yz/okl/");
-    static constexpr const char url4[] =
+    static constexpr char url4[] =
         "/a/bc/def/hijk/{i64}/abcxyz/{i64}/{string}/{u64}/{custom}/x/yz/okl";
     static_assert(UrlParser<url4, is_xyz>::literal_prefix == "/a/bc/def/hijk/");
     static_assert(UrlParser<url4, is_xyz>::literal_suffix == "/x/yz/okl");
-    static constexpr const char url5[] = "//a/bc/def//hijk/{i64}/abcxyz/{i64}/"
-                                         "{string}/{u64}/{custom}/x//yz/okl//";
+    static constexpr char url5[] = "//a/bc/def//hijk/{i64}/abcxyz/{i64}/"
+                                   "{string}/{u64}/{custom}/x//yz/okl//";
     static_assert(UrlParser<url5, is_xyz>::literal_prefix == "//a/bc/def//hijk/");
     static_assert(UrlParser<url5, is_xyz>::literal_suffix == "/x//yz/okl//");
-    static constexpr const char url6[] = "/";
+    static constexpr char url6[] = "/";
     static_assert(UrlParser<url6>::literal_prefix == "/");
     static_assert(UrlParser<url6>::literal_suffix == "/");
-    static constexpr const char url7[] = "/abc/xyz/hohoho";
+    static constexpr char url7[] = "/abc/xyz/hohoho";
     static_assert(UrlParser<url7>::literal_prefix == "/abc/xyz/hohoho");
     static_assert(UrlParser<url7>::literal_suffix == "/abc/xyz/hohoho");
     // Last argument is empty string
-    static constexpr const char url8[] = "/{i64}/x/{string}";
+    static constexpr char url8[] = "/{i64}/x/{string}";
     static_assert(UrlParser<url8>::try_parse("/123/x/") == std::tuple(123, ""));
     static_assert(UrlParser<url8>::try_parse("/123/x/4a02bc") == std::tuple(123, "4a02bc"));
     static_assert(
@@ -72,7 +71,7 @@ TEST(http, UrlParser) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_simple) {
     http::UrlDispatcher<int> ud;
-    static constexpr const char url[] = "/foo/{i64}/bar/{string}/test";
+    static constexpr char url[] = "/foo/{i64}/bar/{string}/test";
     int runs = 0;
     ud.add_handler<url>([&](int64_t a, StringView b) {
         ++runs;
@@ -99,7 +98,7 @@ static auto canonized_collisions(const http::UrlDispatcher<ResponseT>& ud) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_i8) {
     http::UrlDispatcher<int8_t> ud;
-    static constexpr const char url[] = "/{i8}";
+    static constexpr char url[] = "/{i8}";
     ud.add_handler<url>([&](int8_t x) { return x; });
     EXPECT_EQ(ud.dispatch("/-129"), std::nullopt);
     EXPECT_EQ(ud.dispatch("/-128"), -128);
@@ -112,7 +111,7 @@ TEST(http, UrlDispatcher_i8) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_i16) {
     http::UrlDispatcher<int16_t> ud;
-    static constexpr const char url[] = "/{i16}";
+    static constexpr char url[] = "/{i16}";
     ud.add_handler<url>([&](int16_t x) { return x; });
     EXPECT_EQ(ud.dispatch("/-32769"), std::nullopt);
     EXPECT_EQ(ud.dispatch("/-32768"), -32768);
@@ -125,7 +124,7 @@ TEST(http, UrlDispatcher_i16) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_i32) {
     http::UrlDispatcher<int32_t> ud;
-    static constexpr const char url[] = "/{i32}";
+    static constexpr char url[] = "/{i32}";
     ud.add_handler<url>([&](int32_t x) { return x; });
     EXPECT_EQ(ud.dispatch("/-2147483649"), std::nullopt);
     EXPECT_EQ(ud.dispatch("/-2147483648"), -2147483648);
@@ -138,7 +137,7 @@ TEST(http, UrlDispatcher_i32) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_i64) {
     http::UrlDispatcher<int64_t> ud;
-    static constexpr const char url[] = "/{i64}";
+    static constexpr char url[] = "/{i64}";
     ud.add_handler<url>([&](int64_t x) { return x; });
     EXPECT_EQ(ud.dispatch("/-9223372036854775809"), std::nullopt);
     EXPECT_EQ(ud.dispatch("/-9223372036854775808"), -9223372036854775807 - 1);
@@ -151,7 +150,7 @@ TEST(http, UrlDispatcher_i64) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_u8) {
     http::UrlDispatcher<uint8_t> ud;
-    static constexpr const char url[] = "/{u8}";
+    static constexpr char url[] = "/{u8}";
     ud.add_handler<url>([&](uint8_t x) { return x; });
     EXPECT_EQ(ud.dispatch("/-1"), std::nullopt);
     EXPECT_EQ(ud.dispatch("/0"), 0);
@@ -164,7 +163,7 @@ TEST(http, UrlDispatcher_u8) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_u16) {
     http::UrlDispatcher<uint16_t> ud;
-    static constexpr const char url[] = "/{u16}";
+    static constexpr char url[] = "/{u16}";
     ud.add_handler<url>([&](uint16_t x) { return x; });
     EXPECT_EQ(ud.dispatch("/-1"), std::nullopt);
     EXPECT_EQ(ud.dispatch("/0"), 0);
@@ -177,7 +176,7 @@ TEST(http, UrlDispatcher_u16) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_u32) {
     http::UrlDispatcher<uint32_t> ud;
-    static constexpr const char url[] = "/{u32}";
+    static constexpr char url[] = "/{u32}";
     ud.add_handler<url>([&](uint32_t x) { return x; });
     EXPECT_EQ(ud.dispatch("/-1"), std::nullopt);
     EXPECT_EQ(ud.dispatch("/0"), 0);
@@ -190,7 +189,7 @@ TEST(http, UrlDispatcher_u32) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_u64) {
     http::UrlDispatcher<uint64_t> ud;
-    static constexpr const char url[] = "/{u64}";
+    static constexpr char url[] = "/{u64}";
     ud.add_handler<url>([&](uint64_t x) { return x; });
     EXPECT_EQ(ud.dispatch("/-1"), std::nullopt);
     EXPECT_EQ(ud.dispatch("/0"), 0);
@@ -203,9 +202,9 @@ TEST(http, UrlDispatcher_u64) {
 // NOLINTNEXTLINE
 TEST(http, UrlDispatcher_overlapping_patterns) {
     http::UrlDispatcher<int> ud;
-    static constexpr const char url0[] = "/{i64}/{u64}/{u64}/";
-    static constexpr const char url1[] = "/{u64}/{i64}/{u64}/";
-    static constexpr const char url2[] = "/{u64}/{u64}/{i64}/";
+    static constexpr char url0[] = "/{i64}/{u64}/{u64}/";
+    static constexpr char url1[] = "/{u64}/{i64}/{u64}/";
+    static constexpr char url2[] = "/{u64}/{u64}/{i64}/";
     int runs = 0;
     ud.add_handler<url0>([&](int64_t a, uint64_t b, uint64_t c) {
         ++runs;
@@ -248,18 +247,18 @@ TEST(http, UrlDispatcher_all_potential_collisions) {
     http::UrlDispatcher<int> ud;
     using VC = decltype(ud.all_potential_collisions());
 
-    static constexpr const char url0[] = "/a/b/c/{string}/x/y/z/";
+    static constexpr char url0[] = "/a/b/c/{string}/x/y/z/";
     ud.add_handler<url0>([&](StringView /*unused*/) { return 0; });
     EXPECT_EQ(canonized_collisions(ud), VC{});
 
-    static constexpr const char url1[] = "/a/b/{string}/o/{string}/y/z/";
+    static constexpr char url1[] = "/a/b/{string}/o/{string}/y/z/";
     ud.add_handler<url1>([&](StringView /*unused*/, StringView /*unused*/) { return 0; });
 
-    static constexpr const char url2[] = "/a/b/c/o/{string}/y/z/";
+    static constexpr char url2[] = "/a/b/c/o/{string}/y/z/";
     ud.add_handler<url2>([&](StringView /*unused*/) { return 0; });
     EXPECT_EQ(canonized_collisions(ud), (VC{{url2, url0}, {url2, url1}, {url0, url1}}));
 
-    static constexpr const char url3[] = "/a/b/{string}/o/x/y/z/";
+    static constexpr char url3[] = "/a/b/{string}/o/x/y/z/";
     ud.add_handler<url3>([&](StringView /*unused*/) { return 0; });
     EXPECT_EQ(
         canonized_collisions(ud),
@@ -270,7 +269,7 @@ TEST(http, UrlDispatcher_all_potential_collisions) {
             {url0, url1},
             {url3, url1}}));
     // This url does not collide
-    static constexpr const char url4[] = "/z/y/x/{string}/c/b/a/";
+    static constexpr char url4[] = "/z/y/x/{string}/c/b/a/";
     ud.add_handler<url4>([&](StringView /*unused*/) { return 0; });
     EXPECT_EQ(
         canonized_collisions(ud),
@@ -287,7 +286,7 @@ TEST(http, UrlDispatcher_two_handlers_for_the_same_url_without_custom_prasers) {
     http::UrlDispatcher<int> ud;
     using VC = decltype(ud.all_potential_collisions());
 
-    static constexpr const char url[] = "/a/{i64}/b";
+    static constexpr char url[] = "/a/{i64}/b";
     ud.add_handler<url>([&](int64_t /*unused*/) { return 0; });
     ud.add_handler<url>([&](int64_t /*unused*/) { return 0; });
 
@@ -306,7 +305,7 @@ TEST(http, UrlDispatcher_two_handlers_for_the_same_url_with_custom_prasers) {
     http::UrlDispatcher<int> ud;
     using VC = decltype(ud.all_potential_collisions());
 
-    static constexpr const char url[] = "/a/{custom}/b";
+    static constexpr char url[] = "/a/{custom}/b";
     int runs = 0;
     ud.add_handler<url, has_even_length>([&](StringView s) {
         ++runs;
