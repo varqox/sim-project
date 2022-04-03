d91cd4f1a4ef16b2efe343cd59ce2312850a61ce
Krzysztof Małysa <varqox@gmail.com>
2022-04-03 17:46:13 +0200

    fix: fixed invoking src/gen_git_commit_cc.py

--- sip/src/gen_git_commit_cc.py
+++ sip/src/gen_git_commit_cc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
+import sys
 
-commit = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('ascii')
+commit = subprocess.check_output(['git', '--git-dir=' + sys.argv[1], 'rev-parse', 'HEAD']).strip().decode('ascii')
 print(f'extern constexpr auto COMMIT = "{commit}";')
