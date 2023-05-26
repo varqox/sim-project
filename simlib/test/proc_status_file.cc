e230f2a1ad557a46872ca0be6348d9a4f5893ceb
Krzysztof Małysa <varqox@gmail.com>
2023-05-26 18:28:52 +0200

    test: fix proc_status_file test occasional failures

--- simlib/test/proc_status_file.cc
+++ simlib/test/proc_status_file.cc
@@ -1,14 +1,11 @@
-#include <csignal>
 #include <gtest/gtest.h>
 #include <regex>
-#include <simlib/concat_tostr.hh>
-#include <simlib/concurrent/semaphore.hh>
 #include <simlib/ctype.hh>
 #include <simlib/file_contents.hh>
 #include <simlib/file_descriptor.hh>
 #include <simlib/proc_status_file.hh>
 #include <simlib/string_view.hh>
-#include <simlib/syscalls.hh>
+#include <sys/mman.h>
 #include <unistd.h>
 
 struct Field {
@@ -28,41 +25,26 @@ Field field_name_and_value(StringView line) {
 
 // NOLINTNEXTLINE
 TEST(proc_status_file, open_proc_status_and_field_from_proc_status) {
-    auto cpid = fork();
-    if (cpid == 0) {
-        (void)kill(getpid(), SIGSTOP);
-        _exit(0);
-    }
-    ASSERT_NE(cpid, -1);
-    // Parent
-    ASSERT_EQ(syscalls::waitid(P_PID, cpid, nullptr, WSTOPPED, nullptr), 0);
-    FileDescriptor fd = open_proc_status(cpid);
-    auto contents = get_file_contents(fd);
-    {
-        auto normalize = [](auto str) {
-            return std::regex_replace(std::move(str), std::regex{"\nSigQ:.*?\n"}, "\n");
-        };
-        EXPECT_EQ(
-            normalize(contents), normalize(get_file_contents(concat("/proc/", cpid, "/status")))
-        );
-    }
-
+    FileDescriptor fd = open_proc_status(getpid());
+    ASSERT_TRUE(fd.is_open());
+    auto proc_status_contents = get_file_contents(fd);
+    fd = memfd_create("proc_status_file test", MFD_CLOEXEC);
+    ASSERT_TRUE(fd.is_open());
+    ASSERT_EQ(write_all(fd, proc_status_contents), proc_status_contents.size());
     static constexpr auto is_newline = [](char c) { return c == '\n'; };
     static constexpr auto not_newline = [](char c) { return c != '\n'; };
-    auto first_field = field_name_and_value(StringView{contents}.extract_leading(not_newline));
+    auto first_field =
+        field_name_and_value(StringView{proc_status_contents}.extract_leading(not_newline));
     EXPECT_EQ(field_from_proc_status(fd, first_field.name), first_field.value)
         << "first_field.name: " << first_field.name;
 
-    EXPECT_EQ(field_from_proc_status(fd, "Pid"), to_string(cpid));
-    EXPECT_EQ(field_from_proc_status(fd, "Tgid"), to_string(cpid));
-    EXPECT_EQ(field_from_proc_status(fd, "PPid"), to_string(getpid()));
+    EXPECT_EQ(field_from_proc_status(fd, "Pid"), to_string(gettid()));
+    EXPECT_EQ(field_from_proc_status(fd, "Tgid"), to_string(getpid()));
+    EXPECT_EQ(field_from_proc_status(fd, "PPid"), to_string(getppid()));
 
     auto last_field = field_name_and_value(
-        StringView{contents}.without_trailing(is_newline).extract_trailing(not_newline)
+        StringView{proc_status_contents}.without_trailing(is_newline).extract_trailing(not_newline)
     );
     EXPECT_EQ(field_from_proc_status(fd, last_field.name), last_field.value)
         << "last_field.name: " << last_field.name;
-
-    (void)kill(cpid, SIGCONT);
-    ASSERT_EQ(syscalls::waitid(P_PID, cpid, nullptr, WEXITED, nullptr), 0);
 }
